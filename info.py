f3eec653ca974c101e7cc59c989b188489804cc0
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2024-11-17 07:55:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 START_IMG = (environ.get('START_IMG', 'https://envs.sh/5ur.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/289f2ac0537d203d5334b.jpg')).split()
+PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/5uM.jpg')).split()
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/e69de013e2fbbdfb9b34f.jpg'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
